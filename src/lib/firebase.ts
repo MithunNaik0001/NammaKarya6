4734487267d4b53703cfb2ece1b3a6b38b51f7a3
Mithun Naik <mithunnaik521@gmail.com>
2025-10-18 13:52:25 +0000

    set the api key in athentication

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,7 +1,6 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp, getApps } from "firebase/app";
+import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import 'dotenv/config'
 
 // Your web app's Firebase configuration
 // The config is populated with environment variables from your hosting provider.
@@ -15,16 +14,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-let app;
-// Check if Firebase has already been initialized
-if (!getApps().length) {
-  try {
-    app = initializeApp(firebaseConfig);
-  } catch (e) {
-    console.error("Firebase initialization error", e);
-  }
-} else {
-  app = getApps()[0];
-}
+// We check if the app is already initialized to avoid errors.
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 
 export const auth = getAuth(app);
